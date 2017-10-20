9a880677166e8a567f6d53e6c6a917a03833e73d
angelozerr <angelo.zerr@gmail.com>
2017-10-20 05:16:32 +0200

    Check that editor has lsp codelens capability

--- org.eclipse.codelens.lsp4e/src/org/eclipse/codelens/lsp4e/internal/LSPCodeLensControllerProvider.java
+++ org.eclipse.codelens.lsp4e/src/org/eclipse/codelens/lsp4e/internal/LSPCodeLensControllerProvider.java
@@ -1,16 +1,23 @@
 package org.eclipse.codelens.lsp4e.internal;
 
+import java.util.Collection;
+
 import org.eclipse.codelens.editors.DefaultCodeLensController;
 import org.eclipse.codelens.editors.ICodeLensController;
 import org.eclipse.codelens.editors.ICodeLensControllerFactory;
+import org.eclipse.lsp4e.LSPEclipseUtils;
+import org.eclipse.lsp4e.LanguageServiceAccessor;
+import org.eclipse.lsp4e.LanguageServiceAccessor.LSPDocumentInfo;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class LSPCodeLensControllerProvider implements ICodeLensControllerFactory {
 
 	@Override
 	public boolean isRelevant(ITextEditor textEditor) {
-		
-		return true;
+		Collection<LSPDocumentInfo> infos = LanguageServiceAccessor.getLSPDocumentInfosFor(
+				LSPEclipseUtils.getDocument(textEditor),
+				capabilities -> capabilities.getCodeLensProvider() != null);
+		return (!infos.isEmpty());
 	}
 
 	@Override
